462d695736718dedd8ecd0f94db39cf0349cb5bf
programadorisgod <catapiascastro@gmail.com>
2025-03-22 10:45:48 -0500

    V2

--- src/components/ListTask.jsx
+++ src/components/ListTask.jsx
@@ -1,33 +1,22 @@
 import { useState } from "react";
+import { ItemTask } from "./ItemComponet";
+import { useCallback } from "react";
 
-// eslint-disable-next-line react/prop-types
 export function ListTask({ tasks = [] }) {
   const [counter, setCounter] = useState(0);
 
+  const handleInputChange = useCallback(() => {
+    setCounter((prev) => prev - 1);
+  });
   return (
     <>
       {tasks.map((task) => {
         return (
-          <article key={task.id + 1}>
-            <header>
-              <h3>{task.name}</h3>
-            </header>
-
-            <main>
-              <p>{task.complete ? "Completada" : "incompleta"}</p>
-              <p>
-                Prioridad: {task.priority} {Math.random(1)}{" "}
-              </p>
-              <span> date: {task.date}</span>
-            </main>
-
-            <input
-              placeholder="hi"
-              onChange={() => {
-                setCounter((prev) => prev - 1);
-              }}
-            ></input>
-          </article>
+          <ItemTask
+            key={task.id}
+            task={task}
+            handleInputChange={handleInputChange}
+          />
         );
       })}
       <button onClick={() => setCounter((prev) => prev + 1)}>{counter}</button>
